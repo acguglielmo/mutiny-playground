588e4905fbf5dcb5033bd76bec0d888a07efbff5
Antonio Guglielmo <antonio_guglielmo@hotmail.com>
2020-06-27 18:18:05 -0300

    Remove unnecessary code

--- src/test/java/com/acguglielmo/mutinyplayground/ReactiveTest.java
+++ src/test/java/com/acguglielmo/mutinyplayground/ReactiveTest.java
@@ -1,50 +1,42 @@
 package com.acguglielmo.mutinyplayground;
 
+import static java.lang.String.format;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
 public class ReactiveTest {
 
-    private static final Logger LOGGER = LogManager.getLogger(ReactiveTest.class);
-    
-    @RegisterExtension
-    ConsoleWatcherSupport extension = new ConsoleWatcherSupport(LogManager.getLogger(ReactiveTest.class));
-    
     @Test
     public void uniTest() {
-        
+
         Uni.createFrom().item("Antonio")
             .onItem()
-            .apply(n -> String.format("hello %s", n))
+            .apply( e -> format("hello %s", e) )
             .subscribe()
-            .with(LOGGER::info);
-        
-        assertThat(extension.getOutput(), equalTo("hello Antonio\n"));
-    
+            .with( e -> assertThat(e, equalTo("hello Antonio") ) );
+
     }
-    
+
     @Test
     public void multiTest() {
-        
+
         Multi.createFrom()
             .items("Antonio", "Guglielmo", "Test")
             .onItem()
             .apply(n -> String.format("hello %s", n))
             .subscribe()
             .with(
-                    LOGGER::info,
-                    failure -> LOGGER.error("Failed with " + failure.getMessage()));
-        
-        assertThat(extension.getOutput(), equalTo("hello Antonio\nhello Guglielmo\nhello Test\n"));
-        
+            		e -> assertThat(e, equalTo( "hello Antonio" ) ),
+                    failure -> fail(failure)
+            );
+
+
     }
-    
+
 }
